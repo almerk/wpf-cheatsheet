5ed9c0e379dd66c6384a85727afbfa52cdd30fc9
almerk <almerk.work@gmail.com>
2020-12-14 19:07:26 +0300

    Added main method to application

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -8,10 +8,19 @@ using System.Windows;
 
 namespace StudyWPF
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
     public partial class App : Application
     {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThread]
+        [System.Diagnostics.DebuggerNonUserCode]
+        public static void Main()
+        {
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
     }
 }
