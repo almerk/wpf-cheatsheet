1a1ca0320e67fc14c8ff7a4c51dc28fb674a4bc6
almerk <almerk.work@gmail.com>
2020-12-15 14:39:48 +0300

    Added command sample

--- src/ViewModels/Application.cs
+++ src/ViewModels/Application.cs
@@ -1,20 +1,34 @@
 ﻿using StudyWPF.Models;
+using StudyWPF.ViewModels.Commands;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StudyWPF.ViewModels
 {
-    public class Application: Utils.NotifyPropertyChanged
+    public class Application : Utils.NotifyPropertyChanged
     {
+        private bool hasErrors;
+        private WindowLoadedCommand windowLoaded;
         public string Name => "Informer";
-        //private CalendarioContext _context;
-
-        public Application(/*Models.CalendarioContext context*/)
+        public bool HasErrors { get => hasErrors; private set { hasErrors = value; OnPropertyChanged(); } }
+        public ObservableCollection<Error> Errors { get; }
+        public Application()
         {
-            //this._context = context;
+            Errors = new ObservableCollection<Error>();//TODO: DI errors
+            Errors.CollectionChanged += (s, e) => { HasErrors = Errors.Any(); };
         }
+
+        public WindowLoadedCommand WindowLoaded => LazyInitializer.EnsureInitialized(ref windowLoaded, () =>  
+        new WindowLoadedCommand(async () => 
+        {
+            await Task.Delay(1000);
+            Errors.Add(new Error() { Message = "Test error" });
+            Errors.Add(new Error() { Message = "Another test error" });
+        }));
     }
 }
---
+++ src/ViewModels/Commands/WindowLoadedCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StudyWPF.ViewModels.Commands
+{
+    public class WindowLoadedCommand : ICommand
+    {
+        private Action _action;
+
+        public WindowLoadedCommand(Action action) 
+        {
+            this._action = action;
+        }
+        public event EventHandler CanExecuteChanged 
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
